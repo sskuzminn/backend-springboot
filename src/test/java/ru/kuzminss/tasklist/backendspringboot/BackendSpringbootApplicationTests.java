ff7b7827bc0725899f72e441e24640fed9e67297
Sergey Kuzmin <s.s.kuzmin70@yandex.ru>
2023-08-21 22:43:17 +0700

    Initial commit

---
+++ src/test/java/ru/kuzminss/tasklist/backendspringboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzminss.tasklist.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
