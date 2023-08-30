2b481094c9128d0bb79a7cd3a567cd5e529fd315
Sergey Kuzmin <s.s.kuzmin70@yandex.ru>
2023-08-30 22:55:32 +0700

    Добавление @DeleteMapping @GetMapping

--- src/main/java/ru/kuzminss/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/kuzminss/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,6 +1,7 @@
 package ru.kuzminss.tasklist.backendspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,15 +10,18 @@ import ru.kuzminss.tasklist.backendspringboot.entity.Priority;
 import ru.kuzminss.tasklist.backendspringboot.repo.PriorityRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @RestController
 @RequestMapping("/priority") // базовый адрес
 public class PriorityController {
 
+    // доступ к данным из БД
     private PriorityRepository priorityRepository;
 
-
+    // автоматическое внедрение экземпляра класса через конструктор
+    // не используем @Autowired ля переменной класса, т.к. "Field injection is not recommended "
     public PriorityController(PriorityRepository priorityRepository) {
         this.priorityRepository = priorityRepository;
     }
@@ -34,23 +38,82 @@ public class PriorityController {
 
     }
 
+
     @PostMapping("/add")
     public ResponseEntity<Priority> add(@RequestBody Priority priority) {
 
-        //проверка на обязательные параметры
+        // проверка на обязательные параметры
         if (priority.getId() != null && priority.getId() != 0) {
-            return new ResponseEntity("redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
         }
 
-
-        //если передали пустое значение title
+        // если передали пустое значение title
         if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
             return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
         }
 
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
 
+        // save работает как на добавление, так и на обновление
         return ResponseEntity.ok(priorityRepository.save(priority));
     }
+
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Priority priority) {
+
+        // проверка на обязательные параметры
+        if (priority.getId() == null || priority.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение color
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // save работает как на добавление, так и на обновление
+        return ResponseEntity.ok(priorityRepository.save(priority));
+
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+
+        Priority priority = null;
+
+        try {
+            priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException e) { //если id не найдено
+            e.printStackTrace();
+            return new ResponseEntity("id="+id+ " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priority);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+
+
+        try {
+            priorityRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            e.printStackTrace();
+            return new ResponseEntity("id="+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK); // не возвращаем удаленный объект
+    }
+
 
 
 }
